25f4b9f7522233150f9cc6211437c2303e8446de
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2024-08-17 16:44:28 -0700

    fix: stake page

--- pages/stake.tsx
+++ pages/stake.tsx
@@ -1,11 +1,12 @@
 import { Layout, StakingSection, WalletSection } from '@/components';
+import { DEFAULT_CHAIN_NAME } from '@/config';
 import { Divider } from '@interchain-ui/react';
 import { ReactNoSSR } from '@interchain-ui/react-no-ssr';
 
 export default function SingleChain() {
   return (
     <Layout>
-      <WalletSection isMultiChain={false} />
+      <WalletSection isMultiChain={true} providedChainName={DEFAULT_CHAIN_NAME} />
       <Divider height="0.1px" mt="$12" />
       {/* TODO fix type error */}
       {/* Type error: This JSX tag's 'children' prop expects a single child of type 'ReactNode', but multiple children were provided. */}
