e75da8548b17a4d2d0d95e3011d944b3360192d1
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2024-12-03 16:11:51 +0800

    fix(address): add error text for invalid input

--- components/address/AddressSection.tsx
+++ components/address/AddressSection.tsx
@@ -5,76 +5,190 @@ import {
 } from '@interchain-ui/react';
 import converter from "bech32-converting";
 import { useReducer } from 'react';
-import { publicKeyToCosmosAddress, publicKeyToEthereumAddress } from './utils';
+import { mnemonicToKeypair, privateKeyToPublicKey, publicKeyToCosmosAddress, publicKeyToEthereumAddress } from './utils';
 
 type ReducerAction = {
   type: 'update_ethereum_address_hex' |
   'update_ethereum_address_bech32' |
   'update_cosmos_address_hex' |
   'update_cosmos_address_bech32' |
-  'update_public_key';
+  'update_public_key' |
+  'update_private_key' |
+  'update_mnemonic';
   value: string;
 }
 
-function reducer(state: any, action: ReducerAction) {
+type State = {
+  mnemonic: string;
+  privateKey: string;
+  publicKey: string;
+  ethereumAddressHex: string;
+  ethereumAddressBech32: string;
+  cosmosAddressHex: string;
+  cosmosAddressBech32: string;
+  errorText: string;
+}
+
+function reducer(state: State, action: ReducerAction) {
+  let ethereumAddressHex = ''
+  let ethereumAddressBech32 = ''
+  let cosmosAddressHex = ''
+  let cosmosAddressBech32 = ''
+  let publicKey = ''
+  let privateKey = ''
+  let mnemonic = ''
+  let errorText = ''
+
   if (action.value === '') {
-    return { publicKey: '', ethereumAddressHex: '', ethereumAddressBech32: '', cosmosAddressHex: '', cosmosAddressBech32: '' };
+    return {
+      mnemonic,
+      publicKey,
+      privateKey,
+      ethereumAddressHex,
+      ethereumAddressBech32,
+      cosmosAddressHex,
+      cosmosAddressBech32,
+      errorText,
+    };
   }
 
-  // TODO: sanitize inputs further
+  try {
+    switch (action.type) {
+      case 'update_ethereum_address_hex':
+        ethereumAddressHex = action.value
+        ethereumAddressBech32 = converter("nibi").toBech32(ethereumAddressHex)
+        break;
+      case 'update_ethereum_address_bech32':
+        ethereumAddressBech32 = action.value
+        ethereumAddressHex = converter("nibi").toHex(ethereumAddressBech32)
+        break;
+      case 'update_cosmos_address_hex':
+        cosmosAddressHex = action.value
+        cosmosAddressBech32 = converter("nibi").toBech32(cosmosAddressHex)
+        break;
+      case 'update_cosmos_address_bech32':
+        cosmosAddressBech32 = action.value
+        cosmosAddressHex = converter("nibi").toHex(cosmosAddressBech32)
+        break;
+      case 'update_public_key':
+        // keccak-256 hash of the public key
+        publicKey = action.value
 
-  switch (action.type) {
-    case 'update_ethereum_address_hex':
-      return { ethereumAddressHex: action.value, ethereumAddressBech32: converter("nibi").toBech32(action.value), cosmosAddressHex: '', cosmosAddressBech32: '', publicKey: '' };
-    case 'update_ethereum_address_bech32':
-      return { ethereumAddressHex: converter("nibi").toHex(action.value), ethereumAddressBech32: action.value, cosmosAddressHex: '', cosmosAddressBech32: '', publicKey: '' };
-    case 'update_cosmos_address_hex':
-      return { cosmosAddressHex: action.value, cosmosAddressBech32: converter("nibi").toBech32(action.value), ethereumAddressHex: '', ethereumAddressBech32: '', publicKey: '' };
-    case 'update_cosmos_address_bech32':
-      return { cosmosAddressHex: converter("nibi").toHex(action.value), cosmosAddressBech32: action.value, ethereumAddressHex: '', ethereumAddressBech32: '', publicKey: '' };
-    case 'update_public_key':
-      // keccak-256 hash of the public key
-      const ethereumAddressHex = publicKeyToEthereumAddress(action.value)
-      const cosmosAddressHex = publicKeyToCosmosAddress(action.value)
-      return { publicKey: action.value, ethereumAddressHex, ethereumAddressBech32: converter("nibi").toBech32(ethereumAddressHex), cosmosAddressHex, cosmosAddressBech32: converter("nibi").toBech32(cosmosAddressHex) };
-    default:
-      return state;
+        ethereumAddressHex = publicKeyToEthereumAddress(publicKey)
+        ethereumAddressBech32 = converter("nibi").toBech32(ethereumAddressHex)
+
+        cosmosAddressHex = publicKeyToCosmosAddress(publicKey)
+        cosmosAddressBech32 = converter("nibi").toBech32(cosmosAddressHex)
+        break;
+      case 'update_private_key':
+        privateKey = action.value
+        publicKey = privateKeyToPublicKey(privateKey)
+
+        ethereumAddressHex = publicKeyToEthereumAddress(publicKey)
+        ethereumAddressBech32 = converter("nibi").toBech32(ethereumAddressHex)
+
+        cosmosAddressHex = publicKeyToCosmosAddress(publicKey)
+        cosmosAddressBech32 = converter("nibi").toBech32(cosmosAddressHex)
+        break;
+      case 'update_mnemonic':
+        mnemonic = action.value
+        const keypair = mnemonicToKeypair(mnemonic)
+        privateKey = keypair.privateKey
+        publicKey = keypair.compressedPublicKey
+
+        ethereumAddressHex = publicKeyToEthereumAddress(publicKey)
+        ethereumAddressBech32 = converter("nibi").toBech32(ethereumAddressHex)
+
+        cosmosAddressHex = publicKeyToCosmosAddress(publicKey)
+        cosmosAddressBech32 = converter("nibi").toBech32(cosmosAddressHex)
+        break;
+      default:
+        break;
+    }
+  } catch (error) {
+    switch (action.type) {
+      case 'update_mnemonic':
+        errorText = 'Invalid mnemonic'
+        break;
+      case 'update_private_key':
+        errorText = 'Invalid private key'
+        break;
+      case 'update_public_key':
+        errorText = 'Invalid public key'
+        break;
+      case 'update_ethereum_address_hex':
+        errorText = 'Invalid Ethereum address (Hex)'
+        break;
+      case 'update_ethereum_address_bech32':
+        errorText = 'Invalid Ethereum address (Bech32)'
+        break;
+      case 'update_cosmos_address_hex':
+        errorText = 'Invalid Cosmos address (Hex)'
+        break;
+      case 'update_cosmos_address_bech32':
+        errorText = 'Invalid Cosmos address (Bech32)'
+        break;
+    }
   }
+
+  return {
+    mnemonic,
+    privateKey,
+    publicKey,
+    ethereumAddressHex,
+    ethereumAddressBech32,
+    cosmosAddressHex,
+    cosmosAddressBech32,
+    errorText,
+  };
 }
 
 const AddressSection = () => {
-  // const { address } = useChain(DEFAULT_CHAIN_NAME);
-
-  const [addresses, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useReducer(reducer, {
+    mnemonic: '',
+    privateKey: '',
     publicKey: '',
     ethereumAddressHex: '',
     ethereumAddressBech32: '',
     cosmosAddressHex: '',
     cosmosAddressBech32: '',
+    errorText: '',
   });
 
 
   return (
     <>
-      <Text fontSize="$2xl" fontWeight="$semibold" color="$red200" textAlign={'center'}>Address Converter</Text>
-      <Box mb="$8">
+      <Box display="flex" flexDirection={'column'} gap="$8">
+        <Text fontSize="$2xl" fontWeight="$bold" color="$red200" textAlign={'center'}>Address Converter</Text>
+        <Text color="$red500" textAlign={'center'} fontSize={'$xl'} fontWeight={"$semibold"}>{state.errorText}</Text>
+
+        <Text fontSize="$lg" fontWeight="$semibold">Mnemonic</Text>
+        <TextField value={state.mnemonic} id="mnemonic" onChange={(e) => dispatch({ type: 'update_mnemonic', value: e.target.value })} />
+
+        <Text fontSize="$lg" fontWeight="$semibold">Private Key</Text>
+        <TextField value={state.privateKey} id="private key" onChange={(e) => dispatch({ type: 'update_private_key', value: e.target.value })} />
+
         <Text fontSize="$lg" fontWeight="$semibold">Public Key</Text>
-        <TextField value={addresses.publicKey} id="address" onChange={(e) => dispatch({ type: 'update_public_key', value: e.target.value })} />
-      </Box>
-      <Box display="flex" gap="$8">
-        <Box display="flex" gap="$8" flexDirection={'column'} width={'50%'}>
-          <Text fontSize="$lg" fontWeight="$semibold">Ethereum Address (Hex)</Text>
-          <TextField value={addresses.ethereumAddressHex} id="address" onChange={(e) => dispatch({ type: 'update_ethereum_address_hex', value: e.target.value })} />
-          <Text fontSize="$lg" fontWeight="$semibold">Ethereum Address (Bech32)</Text>
-          <TextField value={addresses.ethereumAddressBech32} id="address" onChange={(e) => dispatch({ type: 'update_ethereum_address_bech32', value: e.target.value })} />
+        <TextField value={state.publicKey} id="public key" onChange={(e) => dispatch({ type: 'update_public_key', value: e.target.value })} />
+
+        <Box display="flex" gap="$16">
+          <Box display="flex" gap="$8" flexDirection={'column'} width={'50%'}>
+            {/* <Text fontSize={'$xl'} fontWeight="$semibold">HD Path: <Text color={"$red500"} fontSize="$lg" fontWeight="$semibold" as="span">m/44'/60'/0'/0/0</Text></Text> */}
+            <Text fontSize="$lg" fontWeight="$semibold">Ethereum Address (Hex)</Text>
+            <TextField value={state.ethereumAddressHex} id="ethereum address hex" onChange={(e) => dispatch({ type: 'update_ethereum_address_hex', value: e.target.value })} />
+            <Text fontSize="$lg" fontWeight="$semibold">Ethereum Address (Bech32)</Text>
+            <TextField value={state.ethereumAddressBech32} id="ethereum address bech32" onChange={(e) => dispatch({ type: 'update_ethereum_address_bech32', value: e.target.value })} />
+          </Box>
+          <Box display="flex" flexDirection={'column'} gap="$8" width={'50%'}>
+            {/* <Text fontSize={'$xl'} fontWeight="$semibold">HD Path: <Text color={"$red500"} fontSize="$lg" fontWeight="$semibold" as="span">m/44'/118'/0'/0/0</Text></Text> */}
+            <Text fontSize="$lg" fontWeight="$semibold">Cosmos Address (Hex)</Text>
+            <TextField value={state.cosmosAddressHex} id="cosmos address hex" onChange={(e) => dispatch({ type: 'update_cosmos_address_hex', value: e.target.value })} />
+            <Text fontSize="$lg" fontWeight="$semibold">Cosmos Address (Bech32)</Text>
+            <TextField value={state.cosmosAddressBech32} id="cosmos address bech32" onChange={(e) => dispatch({ type: 'update_cosmos_address_bech32', value: e.target.value })} />
+          </Box>
         </Box>
-        <Box display="flex" flexDirection={'column'} gap="$8" width={'50%'}>
-          <Text fontSize="$lg" fontWeight="$semibold">Cosmos Address (Hex)</Text>
-          <TextField value={addresses.cosmosAddressHex} id="address" onChange={(e) => dispatch({ type: 'update_cosmos_address_hex', value: e.target.value })} />
-          <Text fontSize="$lg" fontWeight="$semibold">Cosmos Address (Bech32)</Text>
-          <TextField value={addresses.cosmosAddressBech32} id="address" onChange={(e) => dispatch({ type: 'update_cosmos_address_bech32', value: e.target.value })} />
-        </Box>
       </Box>
+
     </>
   );
 };
--- components/address/utils.ts
+++ components/address/utils.ts
@@ -1,17 +1,25 @@
 import EthCrypto from 'eth-crypto';
 import { keccak256 } from "ethereum-cryptography/keccak";
 import { ripemd160 } from "ethereum-cryptography/ripemd160";
+import { secp256k1 } from 'ethereum-cryptography/secp256k1';
 import { sha256 } from "ethereum-cryptography/sha256";
 import { bytesToHex, hexToBytes } from "ethereum-cryptography/utils";
+import { HDNodeWallet } from 'ethers';
 
-
+/**
+ * Convert Ethereum public key to Ethereum address
+ * @param publicKey - Ethereum public key. Accepts both compressed and uncompressed public keys.
+ * @returns Ethereum address
+ */
 export function publicKeyToEthereumAddress(publicKey: string): string {
   // Remove '0x' prefix
   let cleanPublicKey = publicKey.replace(/^(0x)?/, '');
 
   if (cleanPublicKey.startsWith('04')) {
+    // compressed
     cleanPublicKey = cleanPublicKey.slice(2);
   } else {
+    // uncompressed
     cleanPublicKey = EthCrypto.publicKey.decompress(cleanPublicKey);
   }
 
@@ -24,11 +32,17 @@ export function publicKeyToEthereumAddress(publicKey: string): string {
   return address;
 }
 
+/**
+ * Convert Ethereum public key to Cosmos address
+ * @param publicKeyHex - Ethereum public key. Accepts both compressed and uncompressed public keys.
+ * @returns Cosmos address
+ */
 export function publicKeyToCosmosAddress(publicKeyHex: string): string {
   // Remove '0x' prefix
   let cleanPublicKey = publicKeyHex.replace(/^(0x)?/, '');
 
   if (cleanPublicKey.startsWith('04')) {
+    // uncompressed
     cleanPublicKey = EthCrypto.publicKey.compress(cleanPublicKey);
   }
 
@@ -39,4 +53,38 @@ export function publicKeyToCosmosAddress(publicKeyHex: string): string {
   const address = '0x' + bytesToHex(hash);
 
   return address;
+}
+
+/**
+ * Convert Ethereum private key to Ethereum public key
+ * @param privateKey - Ethereum private key
+ * @returns Ethereum public key
+ */
+export function privateKeyToPublicKey(privateKey: string): string {
+  const privateKeyBytes = hexToBytes(privateKey);
+  const publicKey = secp256k1.getPublicKey(privateKeyBytes);
+  return bytesToHex(publicKey).toUpperCase();
+}
+
+/**
+ * Convert mnemonic to Keypair
+ * @param mnemonic - Mnemonic
+ * @param hdPath - HD path
+ * @returns Keypair
+ */
+export function mnemonicToKeypair(
+  mnemonic: string,
+  hdPath: string = "m/44'/118'/0'/0/0" // Cosmos default HD path
+): {
+  privateKey: string;
+  compressedPublicKey: string;
+  uncompressedPublicKey: string;
+} {
+  const wallet = HDNodeWallet.fromPhrase(mnemonic, "", hdPath);
+
+  return {
+    privateKey: wallet.privateKey.slice(2).toUpperCase(),
+    compressedPublicKey: wallet.signingKey.compressedPublicKey.slice(2).toUpperCase(),
+    uncompressedPublicKey: wallet.signingKey.publicKey.slice(2).toUpperCase(),
+  };
 }
