bc5885de5ee818aa33d6de3e218294b18df30308
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2024-12-05 08:17:07 +0800

    feat(address): add hdpath and account prefix

--- components/address/AddressSection.tsx
+++ components/address/AddressSection.tsx
@@ -14,7 +14,9 @@ type ReducerAction = {
   'update_cosmos_address_bech32' |
   'update_public_key' |
   'update_private_key' |
-  'update_mnemonic';
+  'update_mnemonic' |
+  'update_hd_path' |
+  'update_account_prefix';
   value: string;
 }
 
@@ -26,10 +28,12 @@ type State = {
   ethereumAddressBech32: string;
   cosmosAddressHex: string;
   cosmosAddressBech32: string;
+  hdPath: string;
+  accountPrefix: string;
   errorText: string;
 }
 
-function reducer(state: State, action: ReducerAction) {
+function reducer(state: State, action: ReducerAction): State {
   let ethereumAddressHex = ''
   let ethereumAddressBech32 = ''
   let cosmosAddressHex = ''
@@ -37,6 +41,8 @@ function reducer(state: State, action: ReducerAction) {
   let publicKey = ''
   let privateKey = ''
   let mnemonic = ''
+  let hdPath = state.hdPath
+  let accountPrefix = state.accountPrefix
   let errorText = ''
 
   if (action.value === '') {
@@ -48,6 +54,8 @@ function reducer(state: State, action: ReducerAction) {
       ethereumAddressBech32,
       cosmosAddressHex,
       cosmosAddressBech32,
+      hdPath,
+      accountPrefix,
       errorText,
     };
   }
@@ -55,52 +63,89 @@ function reducer(state: State, action: ReducerAction) {
   try {
     switch (action.type) {
       case 'update_ethereum_address_hex':
+        accountPrefix = state.accountPrefix
         ethereumAddressHex = action.value
-        ethereumAddressBech32 = converter("nibi").toBech32(ethereumAddressHex)
+        ethereumAddressBech32 = converter(accountPrefix).toBech32(ethereumAddressHex)
         break;
       case 'update_ethereum_address_bech32':
+        accountPrefix = state.accountPrefix
         ethereumAddressBech32 = action.value
-        ethereumAddressHex = converter("nibi").toHex(ethereumAddressBech32)
+        ethereumAddressHex = converter(accountPrefix).toHex(ethereumAddressBech32)
         break;
       case 'update_cosmos_address_hex':
+        accountPrefix = state.accountPrefix
         cosmosAddressHex = action.value
-        cosmosAddressBech32 = converter("nibi").toBech32(cosmosAddressHex)
+        cosmosAddressBech32 = converter(accountPrefix).toBech32(cosmosAddressHex)
         break;
       case 'update_cosmos_address_bech32':
+        accountPrefix = state.accountPrefix
         cosmosAddressBech32 = action.value
-        cosmosAddressHex = converter("nibi").toHex(cosmosAddressBech32)
+        cosmosAddressHex = converter(accountPrefix).toHex(cosmosAddressBech32)
         break;
       case 'update_public_key':
         // keccak-256 hash of the public key
         publicKey = action.value
 
         ethereumAddressHex = publicKeyToEthereumAddress(publicKey)
-        ethereumAddressBech32 = converter("nibi").toBech32(ethereumAddressHex)
+        ethereumAddressBech32 = converter(accountPrefix).toBech32(ethereumAddressHex)
 
         cosmosAddressHex = publicKeyToCosmosAddress(publicKey)
-        cosmosAddressBech32 = converter("nibi").toBech32(cosmosAddressHex)
+        cosmosAddressBech32 = converter(accountPrefix).toBech32(cosmosAddressHex)
         break;
       case 'update_private_key':
         privateKey = action.value
         publicKey = privateKeyToPublicKey(privateKey)
 
         ethereumAddressHex = publicKeyToEthereumAddress(publicKey)
-        ethereumAddressBech32 = converter("nibi").toBech32(ethereumAddressHex)
+        ethereumAddressBech32 = converter(accountPrefix).toBech32(ethereumAddressHex)
 
         cosmosAddressHex = publicKeyToCosmosAddress(publicKey)
-        cosmosAddressBech32 = converter("nibi").toBech32(cosmosAddressHex)
+        cosmosAddressBech32 = converter(accountPrefix).toBech32(cosmosAddressHex)
         break;
       case 'update_mnemonic':
         mnemonic = action.value
-        const keypair = mnemonicToKeypair(mnemonic)
+        const keypair = mnemonicToKeypair(mnemonic, state.hdPath)
         privateKey = keypair.privateKey
         publicKey = keypair.compressedPublicKey
 
         ethereumAddressHex = publicKeyToEthereumAddress(publicKey)
-        ethereumAddressBech32 = converter("nibi").toBech32(ethereumAddressHex)
+        ethereumAddressBech32 = converter(accountPrefix).toBech32(ethereumAddressHex)
 
         cosmosAddressHex = publicKeyToCosmosAddress(publicKey)
-        cosmosAddressBech32 = converter("nibi").toBech32(cosmosAddressHex)
+        cosmosAddressBech32 = converter(accountPrefix).toBech32(cosmosAddressHex)
+        break;
+      case 'update_hd_path':
+        hdPath = action.value
+        mnemonic = state.mnemonic
+        if (mnemonic != '') {
+          const keypair = mnemonicToKeypair(mnemonic, hdPath)
+          privateKey = keypair.privateKey
+          publicKey = keypair.compressedPublicKey
+        } else if (state.privateKey != '') {
+          privateKey = state.privateKey
+          publicKey = privateKeyToPublicKey(privateKey)
+        }
+        ethereumAddressHex = publicKeyToEthereumAddress(publicKey)
+        ethereumAddressBech32 = converter(accountPrefix).toBech32(ethereumAddressHex)
+        cosmosAddressHex = publicKeyToCosmosAddress(publicKey)
+        cosmosAddressBech32 = converter(accountPrefix).toBech32(cosmosAddressHex)
+        break;
+      case 'update_account_prefix':
+        accountPrefix = action.value
+        hdPath = state.hdPath
+        mnemonic = state.mnemonic
+        if (mnemonic != '') {
+          const keypair = mnemonicToKeypair(mnemonic, hdPath)
+          privateKey = keypair.privateKey
+          publicKey = keypair.compressedPublicKey
+        } else if (state.privateKey != '') {
+          privateKey = state.privateKey
+          publicKey = privateKeyToPublicKey(privateKey)
+        }
+        ethereumAddressHex = publicKeyToEthereumAddress(publicKey)
+        ethereumAddressBech32 = converter(accountPrefix).toBech32(ethereumAddressHex)
+        cosmosAddressHex = publicKeyToCosmosAddress(publicKey)
+        cosmosAddressBech32 = converter(accountPrefix).toBech32(cosmosAddressHex)
         break;
       default:
         break;
@@ -128,6 +173,12 @@ function reducer(state: State, action: ReducerAction) {
       case 'update_cosmos_address_bech32':
         errorText = 'Invalid Cosmos address (Bech32)'
         break;
+      case 'update_hd_path':
+        errorText = 'Invalid HD path'
+        break;
+      case 'update_account_prefix':
+        errorText = 'Invalid account prefix'
+        break;
     }
   }
 
@@ -139,6 +190,8 @@ function reducer(state: State, action: ReducerAction) {
     ethereumAddressBech32,
     cosmosAddressHex,
     cosmosAddressBech32,
+    hdPath,
+    accountPrefix,
     errorText,
   };
 }
@@ -146,6 +199,8 @@ function reducer(state: State, action: ReducerAction) {
 const AddressSection = () => {
   const [state, dispatch] = useReducer(reducer, {
     mnemonic: '',
+    hdPath: 'm/44\'/118\'/0\'/0/0',
+    accountPrefix: 'nibi',
     privateKey: '',
     publicKey: '',
     ethereumAddressHex: '',
@@ -192,6 +247,19 @@ const AddressSection = () => {
             <TextField value={state.cosmosAddressHex} id="cosmos address hex" onChange={(e) => dispatch({ type: 'update_cosmos_address_hex', value: e.target.value })} />
             <Text fontSize="$lg" fontWeight="$semibold">Cosmos Address (Bech32)</Text>
             <TextField value={state.cosmosAddressBech32} id="cosmos address bech32" onChange={(e) => dispatch({ type: 'update_cosmos_address_bech32', value: e.target.value })} />
+          </Box>
+        </Box>
+
+        <Box display="flex" gap="$16">
+          <Box display="flex" gap="$8" flexDirection={'column'} width={'50%'}>
+            <Text fontSize="$lg" fontWeight="$semibold">HD Path</Text>
+            <Text fontSize="$sm" color="$gray500">Advanced, only use if you know what you're doing. This is the path to the account in the HD wallet. The default for Nibiru Chain is m/44'/118'/0'/0/0. Ethereum uses m/44'/60'/0'/0/0.</Text>
+            <TextField value={state.hdPath} id="hd path" onChange={(e) => dispatch({ type: 'update_hd_path', value: e.target.value })} />
+          </Box>
+          <Box display="flex" flexDirection={'column'} gap="$8" width={'50%'}>
+            <Text fontSize="$lg" fontWeight="$semibold">Account Prefix</Text>
+            <Text fontSize="$sm" color="$gray500">Advanced, only use if you know what you're doing. This is the prefix for the accoutn address. The default for Nibiru Chain is nibi. Cosmoshub uses cosmos.</Text>
+            <TextField value={state.accountPrefix} id="account prefix" onChange={(e) => dispatch({ type: 'update_account_prefix', value: e.target.value })} />
           </Box>
         </Box>
       </Box>
